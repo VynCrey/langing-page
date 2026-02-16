f3a60931a15d24d0cd260ca3b36df1e47bf95225
VynCrey <vyncrey03@gmail.com>
2026-02-15 22:31:54 -0300

    Cambio de Botones

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,13 +16,15 @@ const Navbar = () => {
             <nav>
                 <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 backgraund-blur-sm">
                 {itemsNavbar.map((item) => (
-                    <div key={item.id}
-                    className={`px-3 py-2 transition duration-150 rounded-full 
-                    cursor-pointer hover:bg-secondary 
-                    ${router === item.link && 'bg-secondary'}`}>
-                        <Link href={item.link}>{item.icon}</Link>
-
-                    </div>
+                    <Link
+                        key={item.id}
+                        href={item.link}
+                        className={`px-3 py-2 transition duration-150 rounded-full 
+                            cursor-pointer hover:bg-secondary 
+                            ${router === item.link && 'bg-secondary'}`}
+                    >
+                        {item.icon}
+                    </Link>
                 ) )}
                 </div>
             </nav>
